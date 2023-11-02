94e2c009bb115c6ddd0e9e786934fe1ddecfd8af
MauroGutierrez53 <mauro.5389@gmail.com>
2023-11-02 20:12:48 -0300

    se agregan comentarios correspondientes

--- models/userModel.js
+++ models/userModel.js
@@ -2,33 +2,29 @@ const {Schema, model} = require('mongoose');
 
 //esquema de Usuario
 const userSchema = new Schema({
- 
-  name: {
+    // se define userName de tipo String  con condicion de requerido y usuario unico 
+  userName: {
     type: String,
     required: true,
+    unique: true,
   },
-  
-  lastName: {
-    type: String,
-    required: true,
-  },
-
+    // se define password de tipo String  con condicion de requerido 
   password: {
     type: String,
     required: true,
   },
-  
+    // se define email de tipo String  con condicion de requerido y usuario unico 
   email: {
     type: String,
     required: true,
     unique: true,
   },  
-
+    // se define contractedPlan de tipo String  con condicion de requerido y usuario unico 
   contractedPlan: {
     type: String,
     required: true,
   },
-
+  // se define admin de tipo Boolean  con  condicion default 
   admin: {
     type: Boolean,
     default: false,
