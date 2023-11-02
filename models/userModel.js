41854d603a36de225d685a0419f36515bd2acdd6
MauroGutierrez53 <mauro.5389@gmail.com>
2023-11-02 20:57:37 -0300

    comentarios en esquema de usuario

--- models/userModel.js
+++ models/userModel.js
@@ -2,33 +2,45 @@ const {Schema, model} = require('mongoose');
 
 //esquema de Usuario
 const userSchema = new Schema({
- 
-  name: {
+  // usuario  de tipo string requerido y unico 
+  userName: {
     type: String,
     required: true,
+    unique: true,
   },
-  
-  lastName: {
-    type: String,
-    required: true,
-  },
-
+  // contraseña de tipo string requerida 
   password: {
     type: String,
     required: true,
   },
-  
+  // email de tipo string requerido
   email: {
     type: String,
     required: true,
     unique: true,
   },  
-
+   // nombres de tipo string requerido
+  name: {
+    type: String,
+    required: true,
+  },
+  //apellido de tipo string requerido 
+  lastName: {
+    type: String,
+    required: true,
+  },
+  //numero de telefono de tipo number requerido
+  phoneNumber: {
+    type: Number,
+    required: true,
+    unique: true,
+  },
+  // plan contratado 
   contractedPlan: {
     type: String,
     required: true,
   },
-
+  // condicion de admin
   admin: {
     type: Boolean,
     default: false,
